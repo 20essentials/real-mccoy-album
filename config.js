5fc034f66a7db6a09a003bdb8d124e00a716b8bf
DominicDev <s0l0estudi4r@gmail.com>
2026-03-28 13:47:13 +0100

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'REAL MCCOY';
+export let nameSongs = [
+	"RUN AWAY", /*1*/
+	"ANOTHER NIGHT", /*2*/
+	"ONE MORE TIME", /*3*/
+	"AUTOMATIC LOVER", /*4*/
+	"LOVE & DEVOTION", /*5*/
+	"COME AND GET YOUR LOVE", /*6*/
+	"OPERATOR", /*7*/
+	"SLEEPING WITH AN ANGEL", /*8*/
+	"OOH BOY", /*9*/
+	" I WANT YOU", /*10*/
+	"I'M OUTTA HERE!", /*11*/
+	"LOVE IS A STRANGER", /*12*/
+	"TAKE A LOOK AT YOUR LIFE", /*13*/
+	"LOVE ALMOST FADED", /*14*/
+	"GIVE A LITTLE LOVE", /*15*/
+	"THE SKY IS THE LIMIT", /*16*/
+	"LOVE SAVE ME", /*17*/
+	"I WANNA COME", /*18*/
+	"MEGABLAST", /*19*/
+	"OPERATOR", /*20*/
+]
